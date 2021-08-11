40f00065b592c1ca5abbf2ab8e204b72bfc076a4
Emily Sousa <diassousa@yahoo.com.br>
2021-08-11 15:32:29 -0300

    Co-authored-by: João Guedes <isudjoao@gmail.com> Co-authored-by: Fellipe Araujo <araujo.fellipe@hotmail.com>

--- src/forca.hs
+++ src/forca.hs
@@ -1,9 +1,19 @@
+import System.IO
+import Data.Char
+
 main =
   do
     let tempLetterSize = 5
     displayHangman
     putStr "  "
     displayLetters tempLetterSize
+    putStr " Digite a letra: "
+    readChar
+
+readChar = 
+  do
+    charInput <- getChar
+    putStrLn ""
 
 displayHangman =
   putStrLn " _ _ _ _ _\n|\n|\n|\n|\n|\n|"
